0220e8f04c158de061f308d9da786c287b2449c1
Alex Golovkin <alexgolowkin@ya.ru>
2018-06-21 16:06:26 +0300

    Added config, added login/register pages

---
+++ inc/config.php
@@ -0,0 +1,8 @@
+<?php
+    //If there is no contant defined called __CONFIG__, do not load this file
+    if (!defined('__CONFIG__')) {
+        exit("You do not have a config file");        
+    }
+
+    // Our config is below
+?>
